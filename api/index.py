af9a06f6ebbca9f65fd00e0d261624bd49989d60
ChuishenX <chuishenx@oblivionocean.top>
2024-03-23 14:47:04 +0800

    First Submission

--- api/index.py
+++ api/index.py
@@ -1,11 +1,41 @@
-from flask import Flask
+from captcha.image import ImageCaptcha
+from flask import Flask, render_template, request
+import string
+import random
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+def generator(name="code.png"):
+    token = string.digits + string.ascii_letters
+    cap = random.sample(token,8)
+    token_str = ''.join(cap)
+    img = ImageCaptcha(width=400,height=150)
+    image = img.generate_image(token_str)
+    image.save(name)
+    return token_str
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/', methods=['GET','POST'])
+def index():
+    global real_str
+    if request.method == 'POST':
+        result = request.form
+        r = result.get('yanzheng').lower() == real_str.lower()
+        if r:
+            return "1"
+        else:
+            return "0"
+    else:
+        with open("index.html", encoding='utf-8') as f:
+            content = f.read()
+        return content
+
+@app.route('/code.png')
+def view():
+    global real_str
+    real_str = generator()
+    with open('code.png', 'rb') as f:
+        content = f.read()
+    return content
+
+# if __name__ == '__main__':
+#     app.run(host='127.0.0.1',port='2333')
